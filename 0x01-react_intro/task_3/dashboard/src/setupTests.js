214c3e56a9c6fc45ac9de095f38a82f656378d15
Lina Abdelkhalig <lelsammani0@gmail.com>
2025-01-18 23:56:18 +0200

    task 7

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,10 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'; // Use the appropriate adapter for your React version
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+global.fetch = require('node-fetch');
+global.TextEncoder = require('util').TextEncoder;
+global.TextDecoder = require('util').TextDecoder;
